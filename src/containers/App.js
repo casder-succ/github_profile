d9da7d6136d18aeba3ed60689d536f248d844f21
casder <casderiopus@gmail.com>
2022-04-26 00:46:46 +0300

    feat: add Profile container to file

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,15 @@
 import logo from '../logo.svg';
 import './App.css';
 import Header from "../components/Header/Header";
+import Profile from "./Profile";
 
 function App() {
-  return (
-    <div className="App">
-      <Header logo={logo} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header logo={logo}/>
+            <Profile/>
+        </div>
+    );
 }
 
 export default App;
