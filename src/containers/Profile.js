64777464bc2ea12834fc12210297cc2922dcf6f3
casder <casderiopus@gmail.com>
2022-04-26 23:07:42 +0300

    feat: add profile avatar

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import './Profile.css';
 
 const Profile = () => {
     const [state, setState] = useState({});
@@ -8,7 +9,7 @@ const Profile = () => {
         async function fetchData() {
             setIsLoading(true);
 
-            const result = await fetch('https://api.github.com/users/octocat');
+            const result = await fetch('https://api.github.com/users/casder-succ');
             const profileJson = await result.json();
 
             if (profileJson) {
@@ -20,18 +21,23 @@ const Profile = () => {
         fetchData();
     }, []);
 
+    if (isLoading) {
+        return <h3>Loading...</h3>;
+    }
+
     return (
-        <div>
-            {isLoading ? <h3>Loading...</h3> : (<ul>
-                <li>avatar_url: {state.avatar_url}</li>
-                <li>html_url: {state.html_url}</li>
-                <li>repos_url: {state.repos_url}</li>
-                <li>name: {state.name}</li>
-                <li>company: {state.company}</li>
-                <li>location: {state.location}</li>
-                <li>email: {state.email}</li>
-                <li>bio: {state.bio}</li>
-            </ul>)}
+        <div className="Profile-wrap">
+            <img className="Profile-avatar" src={state.avatar_url} alt=""/>
+            <ul>
+                <li><strong>avatar_url:</strong> {state.avatar_url}</li>
+                <li><strong>html_url:</strong> {state.html_url}</li>
+                <li><strong>repos_url:</strong> {state.repos_url}</li>
+                <li><strong>name:</strong> {state.name}</li>
+                <li><strong>company:</strong> {state.company}</li>
+                <li><strong>location:</strong> {state.location}</li>
+                <li><strong>email:</strong> {state.email}</li>
+                <li><strong>bio:</strong> {state.bio}</li>
+            </ul>
         </div>
     );
 };
