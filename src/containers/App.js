3ba0a94fe4bef1976facfe23c2f89db635a2890c
casder <casderiopus@gmail.com>
2022-04-28 11:48:28 +0300

    feat: add global styles removed all css files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,31 @@
 import Header from "../components/Header/Header";
 import Profile from "./Profile";
-import styled from "styled-components";
+import styled, {createGlobalStyle} from "styled-components";
 
 const AppWrapper = styled.div`
   text-align: center;
 `
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
+
 function App() {
     return (
-        <AppWrapper>
-            <Header />
-            <Profile/>
-        </AppWrapper>
+        <>
+            <GlobalStyle/>
+            <AppWrapper>
+                <Header/>
+                <Profile/>
+            </AppWrapper>
+        </>
     );
 }
 
